4be03587ab2247439e8c2842177450460e99b2f0
admin-farhan <muhfarhan912391@gmail.com>
2022-10-26 17:53:03 +0700

    typescript day 1

---
+++ MFarhanF_typescript_day3.js
@@ -0,0 +1,5 @@
+function combination(arg1, arg2, arg3) {
+    var output = "".concat(arg1, " ").concat(arg2, " ").concat(arg3);
+    return output;
+}
+console.log(combination(1, 2, "hello"));
---
+++ MFarhanF_typescript_day3.ts
@@ -0,0 +1,6 @@
+function combination(arg1:any, arg2:any, arg3:any):any{
+    var output = `${arg1} ${arg2} ${arg3}`; 
+    return output;
+}
+
+console.log(combination(1,2,"hello"))
