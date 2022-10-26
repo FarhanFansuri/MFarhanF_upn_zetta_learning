ad14f8ec7db9f394324979a4cf108f09715a6672
admin-farhan <muhfarhan912391@gmail.com>
2022-10-26 17:56:04 +0700

    typescript day 1

--- MFarhanF_typescript_day3.ts
+++ MFarhanF_typescript_day3.ts
@@ -1,4 +1,4 @@
-function combination(arg1:any, arg2:any, arg3:any):any{
+function combination(arg1:any, arg2:any, arg3:any):string{
     var output = `${arg1} ${arg2} ${arg3}`; 
     return output;
 }
