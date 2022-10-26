cfeea806cdc54fd0e1709c9e53aa5e58e2739eb7
admin-farhan <muhfarhan912391@gmail.com>
2022-10-26 17:29:45 +0700

    typescript day 2

--- MFarhanF_typescript_day2.js
+++ MFarhanF_typescript_day2.js
@@ -7,5 +7,14 @@ function concatination(data) {
 }
 var data1 = [1, "data", 3, "result"];
 var data2 = ["Bejo", "has", 4, "sport", "cars"];
+var data3 = [1, 2, 3, 4, 5];
+var data4 = ["nama", "saya", "farhan"];
+function getData(data) {
+    data.forEach(function (element) {
+        console.log(element);
+    });
+}
 console.log(concatination(data1));
 console.log(concatination(data2));
+getData(data3);
+getData(data4);
--- MFarhanF_typescript_day2.ts
+++ MFarhanF_typescript_day2.ts
@@ -8,7 +8,17 @@ data.forEach((element) => {
 
 var data1:any[] = [1, "data", 3, "result"];
 var data2:any[] = ["Bejo","has", 4, "sport", "cars"];
+var data3:number[] = [1,2,3,4,5];
+var data4:string[] = ["nama", "saya", "farhan"];
+
+function getData(data:string[]|number[]){
+    data.forEach(element => {
+        console.log(element);
+    });
+}
 
 console.log(concatination(data1));
 console.log(concatination(data2));
 
+getData(data3);
+getData(data4);
