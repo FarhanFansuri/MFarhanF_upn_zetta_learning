b357f5c8d2109d600e7eb687006b2d1f2c8fc0ce
admin-farhan <muhfarhan912391@gmail.com>
2022-11-01 17:43:08 +0700

    pelatihan angular day 4

--- angular_learning/src/app/app.component.ts
+++ angular_learning/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular_learning';
+export class AppComponent implements OnChanges,OnInit{
+  nama  = "Nezuko Kamado";
+  color = "black";
+  berubah = false;
+  deskripsi="Some quick example text to build on the card title and make up the bulk of the card's content.";
+ngOnInit(): void {
+
+console.log("hello World")
+}
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log("berubah");
+  }
+change_value(){
+this.berubah=true;
+}
+change_color(warna:string){
+  this.color=warna;
+}
 }
