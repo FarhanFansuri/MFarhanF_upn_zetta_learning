38fffd02ddd35663924afafffcb7da9dd6d17861
admin-farhan <muhfarhan912391@gmail.com>
2022-11-08 09:55:24 +0700

    pelatihan angular day 9

--- angular_learning/src/app/add-profile/add/add.component.ts
+++ angular_learning/src/app/add-profile/add/add.component.ts
@@ -1,8 +1,9 @@
 import { DataService } from './../../data-servicee/data.service';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ThisReceiver } from '@angular/compiler';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -15,25 +16,45 @@ export class AddComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private dataservice: DataService, private router: Router) { }
   profileForm = this.formBuilder.group({
     IdNumber: new FormControl(null),
-    Name: new FormControl(null),
-    Age: new FormControl(null),
+    Name: new FormControl(null, Validators.pattern("^[A-Z, a-z]*$")),
+    Age: new FormControl(null, [Validators.required,
+    Validators.pattern("^[0-9]*$"),
+    Validators.min(11),]),
     Gender: new FormControl(null),
-    Email: new FormControl(null),
+    Email: new FormControl(null, Validators.email),
     Position: new FormControl(null),
     MaritalStatus: new FormControl(null),
     AddressData: new FormArray([])
   })
 
   data: any[] = [];
+  status: any;
   addData: any = (<FormArray>this.profileForm.get('AddressData')).controls
   ngOnInit(): void {
     this.data = this.dataservice.data
+    this.profileForm.statusChanges.subscribe((change) => {
+      this.status = change
+      console.log(this.status)
+    })
   }
   kirim() {
-    this.dataservice.addData(this.profileForm.value);
-    this.router.navigate(['']);
+    if (this.status == "INVALID") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Data tidak Valid'
+      })
+    } else {
+      Swal.fire(
+        'Good job!',
+        'success'
+      )
+      setTimeout(() => {
+        this.dataservice.addData(this.profileForm.value);
+        this.router.navigate(['']);
+      }, 3000)
+    }
   }
-
   addAddress() {
     (<FormArray>this.profileForm.get('AddressData')).push(new FormGroup({
       Address: new FormControl(null),
