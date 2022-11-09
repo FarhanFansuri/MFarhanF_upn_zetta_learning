d3c0bcced288cd722af0a6f7eee8c92feb839def
admin-farhan <muhfarhan912391@gmail.com>
2022-11-10 05:16:48 +0700

    pelatihan angular day 11

--- angular_learning/src/app/app-routing.module.ts
+++ angular_learning/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ListDataComponent } from './list/list-data/list-data.component';
+import { EditDataComponent } from './edit/edit-data/edit-data.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit/:id', component: EditDataComponent },
+  { path: '', component: ListDataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
